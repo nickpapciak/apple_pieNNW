9c195f3e64fcd60920a7f0d7e00dfe4309e09c15
Nicholas Papciak <nicholaspapciak@gmail.com>
2023-01-24 12:39:48 -0500

    refactoring and code-style stuff

--- Network.py
+++ Network.py
@@ -1,5 +1,8 @@
-import numpy as np 
+from dataclasses import dataclass
+from typing import Callable
 import random 
+
+import numpy as np 
 
 # TODO: use python typing to do actual types 
 # TODO: implement momentum
@@ -9,52 +12,78 @@ import random
 # TODO: different activations, ReLU, LReLU
 
 # assorted functions
-def sigmoid(x): 
+def sigmoid(x: np.ndarray): 
+    """Sigmoid"""
     return 1 / (1 + np.exp(-x))
 
-def dsigmoid(x): 
+def d_sigmoid(x: np.ndarray): 
+    """Derivative of Sigmoid"""
     return sigmoid(x)*(1-sigmoid(x))
 
-def mean_squared_error(expected, actual): 
+def ReLU(x: np.ndarray): 
+    """Rectified Linear Unit"""
+    return x * (x > 0)
+
+def d_ReLU(x: np.ndarray): 
+    """Derivative of Rectified Linear Unit"""
+    return 1 * (x > 0)
+
+
+def mse(expected: np.ndarray, actual: np.ndarray): 
+    """Mean Squared Error"""
     return 0.5*np.linalg.norm(expected - actual)**2
 
-def dmean_squared_error(expected, actual): 
+def d_mse(expected: np.ndarray, actual: np.ndarray): 
+    """Derivative of Mean Squared Error"""
     return expected - actual
 
+@dataclass
+class TrainingData:
+    training: np.ndarray
+    label: np.ndarray
+
 class Network: 
-    def __init__(
-        self, shape, data, truth, 
-        activation_function=sigmoid, 
-        dactivation_function=dsigmoid, 
-        cost_function=mean_squared_error, 
-        dcost_function=dmean_squared_error
-        ): 
+    def __init__(self, shape: list[int], 
+                training: np.ndarray, 
+                labels: np.ndarray, 
+                probe_training: np.ndarray=None, 
+                probe_labels: np.ndarray=None, 
+                suppress_print=False, 
+                activation=sigmoid, 
+                d_activation=d_sigmoid, 
+                cost=mse, d_cost=d_mse): 
         """Initializes a new network."""
         if len(shape) < 2: 
             raise ValueError("Network needs at least 2 layers")
 
-        if (shape[0] != data.shape[1]) or (shape[-1] != truth.shape[1]): 
+        if (shape[0] != training.shape[1]) or (shape[-1] != labels.shape[1]): 
             raise ValueError("Network shape does not match at inputs or outputs")
 
-        if any((
-            len(data.shape) != 3, 
-            len(truth.shape) != 3, 
-            data.shape[2] != 1, 
-            truth.shape[2] != 1, 
-            data.shape[0] != truth.shape[0]
-            )): 
+        conditions = [
+            len(training.shape)  == 3, 
+            len(labels.shape)  == 3,
+            training.shape[2] == 1,
+            labels.shape[2] == 1,
+            training.shape[0] == labels.shape[0]
+            ]
+        if not all(conditions): 
             raise ValueError(
                 "Input data and expected outputs should be 3 dimensional ndarrays \
                 with the shape: (number of total examples, individual data size, 1)"
             )
 
-        self.shape = shape
-        self.data = data
-        self.truth = truth
-        self.activation_function = activation_function
-        self.dactivation_function = dactivation_function
-        self.cost_function = cost_function
-        self.dcost_function = dcost_function
+        self.data = [TrainingData(t, l) for t, l in zip(training, labels)]
+        if probe_training is None or probe_labels is None: 
+            self.probe = None
+        else:
+            self.probe = [TrainingData(t, l) for t, l in zip(probe_training, probe_labels)]
+
+        self.shape : list[int] = shape
+        self.suppress_print : bool = suppress_print
+        self.activation : Callable = activation
+        self.d_activation : Callable = d_activation
+        self.cost : Callable = cost
+        self.d_cost : Callable = d_cost
 
         # creates empty lists which store weight 
         # matrices, and bias and activation vectors
@@ -71,19 +100,19 @@ class Network:
         """Number of layers in the network."""
         return len(self.shape) - 1
 
-    def _forwardprop(self, data): 
-        """Propogrates data forward through the network to set activations."""
+    def _forwardprop(self, _input: np.ndarray): 
+        """Propogrates a datapoint forward through the network to set activations."""
         # sets the input neurons to the input data
-        self.activations[0] = data
+        self.activations[0] = _input
         # loops through each layer
         for l in range(len(self)): 
             # calculates the weighted sum and sets next activations based on activation function
-            self.activations[l + 1] = self.activation_function(self.weights[l] @ self.activations[l] + self.biases[l])
+            self.activations[l + 1] = self.activation(self.weights[l] @ self.activations[l] + self.biases[l])
 
-    def _backprop(self, data, truth): 
+    def _backprop(self, datapoint: TrainingData): 
         """Propogrates backwards through the network to find gradients."""
         # forward propogation
-        self._forwardprop(data)
+        self._forwardprop(datapoint.training)
 
         grad_biases = [None] * len(self)  
         grad_weights = [None] * len(self) 
@@ -92,10 +121,10 @@ class Network:
         # runs backwards through the network
         for l in range(1, len(self) + 1): 
             # activation error is different on last layer
-            a_err = self.dcost_function(self.activations[-1], truth) if l == 1 else (self.weights[-l+1].T @ errors[-l+1])
+            a_err = self.d_cost(self.activations[-1], datapoint.label) if l == 1 else (self.weights[-l+1].T @ errors[-l+1])
             
             # error = dc/dz = dc/da * da/dz = a_err * dactiviation(z)
-            errors[-l] = a_err * self.dactivation_function(self.weights[-l] @ self.activations[-l-1] + self.biases[-l])
+            errors[-l] = a_err * self.d_activation(self.weights[-l] @ self.activations[-l-1] + self.biases[-l])
 
             # computes the change in weights
             grad_weights[-l] = errors[-l] @ self.activations[-l-1].T
@@ -103,54 +132,61 @@ class Network:
 
         return grad_weights, grad_biases
 
-    def _batch_train(self, batch_data, batch_truth, learning_rate): 
+    def _train_batch(self, data: list[TrainingData], learning_rate): 
         """Trains the network on a certain batch of data."""
         # sets empty lists based on weight matrices and bias vectors
         sum_grad_weights =  [np.zeros(W.shape) for W in self.weights]
         sum_grad_biases =  [np.zeros(b.shape) for b in self.biases]
 
         # calculates the sum of the gradients for the batch
-        for data, truth in zip(batch_data, batch_truth): 
-            grad_weights, grad_biases = self._backprop(data, truth)
+        for datapoint in data: 
+            grad_weights, grad_biases = self._backprop(datapoint)
             sum_grad_weights = [mgw + gw for mgw, gw in zip(sum_grad_weights, grad_weights)]
             sum_grad_biases = [mgb + gb for mgb, gb in zip(sum_grad_biases, grad_biases)]
 
         # gradient descent
-        k =  learning_rate/len(batch_data)
+        k =  learning_rate/len(data)
         self.weights = [w - k*mgw for w, mgw in zip(self.weights, sum_grad_weights)]
         self.biases = [b - k*mgb for b, mgb in zip(self.biases, sum_grad_biases)]
 
-    def train(self, epochs, batch_size, learning_rate, probe_data=None, probe_truth=None):
+
+    def train(self, epochs, batch_size, learning_rate):
         """Uses stochastic gradient descent to train the network"""
         if (batch_size > len(self.data)): 
             raise ValueError("Batch size too large")
 
-        for e in range(1, epochs + 1):
-            # splits the data into random smaller batches and then trains
-            # on these smaller random samples to approximate the gradient
-            shuffled_data = list(zip(self.data, self.truth))
-            random.shuffle(shuffled_data)
-            batch_data, batch_truth = zip(*shuffled_data) # undoes the zip
-            
-            # chunks the data and the truth into batches of size `batch size`
-            for i in range(0, len(batch_data), batch_size): 
-                self._batch_train(batch_data[i:i+batch_size], batch_truth[i:i+batch_size], learning_rate)
-            
-            # prints the epoch
-            print(f"Epoch {e}") if probe_data is None and probe_truth is None else \
-            print(f"Epoch {e} ({100*self.evaluate(probe_data, probe_truth)/len(probe_data):.2f}%) \
-                    Cost ({self.cost(batch_data, batch_truth):.2f})"
-            )
+        # trains over multiple epochs
+        for epoch in range(1, epochs + 1):
+            shuffled_data = random.sample(self.data, len(self.data))     
 
-    def classify(self, data): 
+            # we splits the data into shuffled batches to get an
+            # approxiate gradient to speed up the learning process
+            for i in range(0, len(shuffled_data), batch_size): 
+                self._train_batch(shuffled_data[i:i+batch_size], learning_rate)
+
+            if not self.suppress_print: 
+                print(self._summarize(epoch, epochs))
+
+
+
+    def _summarize(self, epoch, total_epochs): 
+        """Gives a string representation of an epoch"""
+        if self.probe is None: 
+            return f"Epoch {epoch}/{total_epochs}"
+        return f"epoch {epoch}/{total_epochs} - accuracy: {self.accuracy(self.probe):.2f}% - loss: {self.loss():.2f}"
+
+    def classify(self, datapoint): 
         """Classifies data by running it through the network."""
-        self._forwardprop(data)
+        self._forwardprop(datapoint)
         return self.activations[-1]
 
-    def evaluate(self, batch_data, batch_truth):
-        """The number of correct evaluations from a batch of data."""
-        return sum([np.argmax(self.classify(d)) == np.argmax(t) for d, t in zip(batch_data, batch_truth)])
+    def accuracy(self, data: list[TrainingData]):
+        """The percentage of correct evaluations from a batch of data."""
+        correct = [np.argmax(self.classify(d.training)) == np.argmax(d.label) for d in data]
+        return  100*np.average(correct)
 
-    def cost(self, data, expected): 
-        """The current network cost from a batch of data."""
-        return self.cost_function(self.classify(data), expected)
+
+    def loss(self): 
+        """The average cost from a batch of data."""
+        return np.average([self.cost(self.classify(d.training), d.label) for d in self.data])
+ 
--- main.py
+++ main.py
@@ -2,13 +2,5 @@ from MNIST import labels, images
 from Network import Network
 
 if __name__ == '__main__': 
-
-    training_data = images[:50_000]
-    training_truths = labels[:50_000]
-
-    probing_data = images[50_000:]
-    probing_truths = labels[50_000:]
-
-    nn = Network([784, 50, 50, 10], data=training_data, truth=training_truths)
-
-    nn.train(epochs=100, batch_size=10, learning_rate=5, probe_data=probing_data, probe_truth=probing_truths)
+    nn = Network([784, 40, 40, 10], training= images[:50_000], labels=labels[:50_000], probe_training=images[50_000:], probe_labels=labels[50_000:])
+    nn.train(epochs=10, batch_size=10, learning_rate=5)
